f64eea781b7bd2b1d205db77d8ad1ed5377db00d
Carl Hembrough <carlhuk@gmail.com>
2017-01-31 11:10:15 +0000

    Updated document model to have a dynamic body field. Added dataset specific metadata model.

--- model/document.go
+++ model/document.go
@@ -2,8 +2,54 @@ package model
 
 // Document contains the base properties for all documents stored in Elastic Search.
 type Document struct {
-	ID    string `json:"id"`
-	Type  string `json:"type"`
+	ID   string      `json:"id"`
+	Type string      `json:"type"`
+	Body interface{} `json:"body"`
+}
+
+type Dataset struct {
+	ID          string       `json:"id"`
+	Title       string       `json:"title"`
+	Description string       `json:"description"`
+	URL         string       `json:"url,omitempty"`
+	Metadata    *Metadata    `json:"metadata,omitempty"`
+	Dimensions  []*Dimension `json:"dimensions,omitempty"`
+}
+
+type Metadata struct {
+	Description        string         `json:"description,omitempty"`
+	Taxonomies         []string       `json:"taxonomies,omitempty"`
+	Contact            *Contact       `json:"contact"`
+	ReleaseDate        string         `json:"releaseDate"`
+	NextRelease        string         `json:"nextReleaseDate"`
+	NationalStatistics bool           `json:"nationalStatistics"`
+	Publications       []string       `json:"associatedPublications"`
+	Methodology        []*Methodology `json:"methodology"`
+	TermsAndConditions string         `json:"termsAndConditions,omitempty"`
+}
+
+type Contact struct {
+	Name  string `json:"name"`
+	Email string `json:"email,omitempty"`
+	Phone string `json:"phone,omitempty"`
+}
+
+type Methodology struct {
 	Title string `json:"title"`
 	URL   string `json:"url"`
+}
+
+type Dimension struct {
+	ID             string             `json:"id"`
+	Name           string             `json:"name"` // Sex
+	Type           string             `json:"type"` // Dimension type
+	Options        []*DimensionOption `json:"options,omitempty"`
+	SelectedOption *DimensionOption   `json:"selectedOption,omitempty"`
+}
+
+type DimensionOption struct {
+	ID   string `json:"id"`
+	Name string `json:"name"` // Male
+
+	Options []*DimensionOption `json:"options,omitempty"`
 }
